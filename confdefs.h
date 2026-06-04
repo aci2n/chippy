88c9c45bfee1e75402c2ec2734da283a5a04f668
aci2n <alvarocalace@hotmail.com>
2026-06-04 11:22:36 -0300

    remove more

--- confdefs.h
+++
@@ -1,9 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "chippy"
-#define PACKAGE_TARNAME "chippy"
-#define PACKAGE_VERSION "0.1.0"
-#define PACKAGE_STRING "chippy 0.1.0"
-#define PACKAGE_BUGREPORT ""
-#define PACKAGE_URL ""
-#define PACKAGE "chippy"
-#define VERSION "0.1.0"
